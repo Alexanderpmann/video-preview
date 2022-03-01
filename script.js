8ba0f010c9af3ba53ed02a0142a8d465e9a0b804
Alexanderpmann <99506481+Alexanderpmann@users.noreply.github.com>
2022-03-01 03:46:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("page loaded...");
+
+function showAlert() {
+    alert("Subscribed!")
+}
+
+function over(element) {
+    element.style.backgroundColor = "red";    
+}
+    
+function out(element) {
+    element.style.backgroundColor = "red";   
+}
+
+
+// document.getElementById('my-video').muted(); 
+// document.getElementById("video").defaultMuted = true;
+
+// function playVid() {
+//     x.play;
+// }
+
+// function pauseVid() {
+//     x.pause();
+// }
+
+
+}
