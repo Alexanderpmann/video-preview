61745d864d1df61a3f4f1a64fce39eef73ebc03d
Alexanderpmann <99506481+Alexanderpmann@users.noreply.github.com>
2022-03-01 03:52:53 -0500

    fixed script - subscribed wasn't functioning

--- script.js
+++ script.js
@@ -11,18 +11,3 @@ function over(element) {
 function out(element) {
     element.style.backgroundColor = "red";   
 }
-
-
-// document.getElementById('my-video').muted(); 
-// document.getElementById("video").defaultMuted = true;
-
-// function playVid() {
-//     x.play;
-// }
-
-// function pauseVid() {
-//     x.pause();
-// }
-
-
-}
